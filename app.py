80bec09e2ee388cae38392cc4834e96c0e0c0356
ricardocarva <ricardofecarva@gmail.com>
2023-04-13 12:01:08 -0400

    Survey UI completed. Update and Delete Info UI done.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 
 app = Flask(__name__)
 
@@ -19,7 +19,8 @@ def home():
         # Check if the email and password match some stored credentials
         if email == 'user@example.com' and password == 'password123':
             # If the credentials are valid, redirect to the student home page
-            return render_template('student-home.html')
+            # return render_template('student-home.html')    
+            return redirect(url_for('student_home'))        
         else:
             # If the credentials are invalid, show an error message
             error_message = 'Invalid email or password'
@@ -42,6 +43,7 @@ def register():
         print(name, flush=True)
         print("Address:", address)
         print("Phone Number:", phone)
+        print("Email:", email)
         print("School:", school)
         print("Year:", year)
         print("Date of Birth:", dob)
@@ -56,26 +58,114 @@ def register():
 
 @app.route('/student-home', methods=['GET'])
 def student_home():
-    return render_template('student-home.html')
+    name = "First Last"
+    print(name)
+    return render_template('student-home.html', name=name)
 
 
 @app.route('/survey', methods=['GET', 'POST'])
 def survey():
     if request.method == "POST":
-        isDepressed = request.form.get("is-feeling-depressed")
-        hasPreviousDiagnostic = request.form.get("previous-diagnostic")
+        depressedMood = request.form.get("depressed-mood")
+        depressedHopeless = request.form.get("depressed-hopeless")
+        lossOfInterestAndEnjoyment = request.form.get("loss-interest-and-enjoyment")
+        lossOfPleasureAndEnjoyment = request.form.get("loss-pleasure-and-enjoyment")
+        lessenedEnergy = request.form.get("fatigued-and-lack-of-energy")
+        lessenedActive = request.form.get("less-active-and-productive")
+        reducedDecisionMaking = request.form.get("reduced-decision-making")
+        reducedConcentration = request.form.get("reduced-concentration")
+        reducedSelfConfidence = request.form.get("reduced-self-confidence")
+        reducedSelfEsteem = request.form.get("reduced-self-steem")
+        ideasOfGuilt = request.form.get("ideas-of-guilt")
+        ideasOfUnworthiness = request.form.get("ideas-of-unworthiness")
+        bleakViewsOfTheFuture = request.form.get("bleack-views-of-the-future")
+        pessimisticViewsOfTheFuture = request.form.get("pessimistic-views-of-the-future")
+        ideasOrActsOfSelfHarmOrSuicide = request.form.get("ideas-or-acts-of-self-harm-or-suicide")
+        disturbedSleep = request.form.get("disturbed-sleep")
+        diminishedAppetite = request.form.get("diminished-apetite")
+        understandingParent = request.form.get("understanding-parent")
+        missedClasses = request.form.get("missed-classes")
+        smokeDrink = request.form.get("smoke-drink")
+        lostRelative = request.form.get("lost-relative")
+        relationshipTrouble = request.form.get("relationship-trouble")
+        plagrisedHw = request.form.get("plagiarized-homework")
+        leftJob = request.form.get("left-job")
+        takingMedication = request.form.get("taking-medication")
+        diagnosedBefore = request.form.get("diagnosed-before")
 
         #To-DO store the data in a database or file
-
-        print(isDepressed, flush=True)
-        print("Has Previous Diagnostic?", hasPreviousDiagnostic)
+        print("depressedMood: ", depressedMood)
+        print("depressedHopeless: ", depressedHopeless)
+        print("lossOfInterestAndEnjoyment: ", lossOfInterestAndEnjoyment)
+        print("lossOfPleasureAndEnjoyment: ", lossOfPleasureAndEnjoyment)
+        print("lessenedEnergy: ", lessenedEnergy)
+        print("lessenedActive: ", lessenedActive)
+        print("reducedDecisionMaking: ", reducedDecisionMaking)
+        print("reducedConcentration: ", reducedConcentration)
+        print("reducedSelfConfidence: ", reducedSelfConfidence)
+        print("reducedSelfEsteem: ", reducedSelfEsteem)
+        print("ideasOfGuilt: ", ideasOfGuilt)
+        print("ideasOfUnworthiness: ", ideasOfUnworthiness)
+        print("bleakViewsOfTheFuture: ", bleakViewsOfTheFuture)
+        print("pessimisticViewsOfTheFuture: ", pessimisticViewsOfTheFuture)
+        print("ideasOrActsOfSelfHarmOrSuicide: ", ideasOrActsOfSelfHarmOrSuicide)
+        print("disturbedSleep: ", disturbedSleep)
+        print("diminishedAppetite: ", diminishedAppetite)
+        print("understandingParent: ", understandingParent)
+        print("missedClasses: ", missedClasses)
+        print("smokeDrink: ", smokeDrink)
+        print("lostRelative: ", lostRelative)
+        print("relationshipTrouble: ", relationshipTrouble)
+        print("plagrisedHw: ", plagrisedHw)
+        print("leftJob: ", leftJob)
+        print("takingMedication: ", takingMedication)
+        print("diagnosedBefore: ", diagnosedBefore)
         # TO DO: 
         # Create student obj using the variables above, insert it to your data structure created before @app.route('/')
         # Return its ID to the variable below 
         ID = "0000001"
 
-        return render_template('succesfully-registered.html', name = isDepressed, ID=ID)
+        name = "student name"
+
+        return render_template('survey-submitted.html', name = name, ID=ID)
     return render_template('survey.html')
+
+@app.route('/deleted', methods=['GET'])
+def account_deleted():
+    return render_template('account-deleted.html')
+
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    name = "First Last"
+    address = "Gainesville, FL 32611"
+    phone = "012-345-6789"
+    email = "email@ufl.edu"
+    school = "University Of Florida"
+    year = "Freshman"
+    dob = "2000-01-01" #Must be in this format!!
+    ID = "0000001"
+
+    if request.method == 'POST':
+        action = request.form['action']
+        if action == 'update':
+            # handle update action
+            print("update")
+            address = request.form.get("address")
+            phone = request.form.get("phone")
+            year = request.form.get("year")
+
+            #TO DO:
+            # Update user profile
+            return render_template('profile.html', name=name, address=address, phone=phone,email=email, school=school,year=year, dob=dob, ID = ID)    
+
+        elif action == 'delete':
+            #TO DO:
+            # Delete user profile
+            print("delete")
+            return render_template('account-deleted.html')
+        elif action == 'back':
+            return redirect(url_for('student_home'))        
+    return render_template('profile.html', name=name, address=address, phone=phone,email=email, school=school,year=year, dob=dob, ID = ID)
 
 if __name__ == '__main__':    
     app.run()
